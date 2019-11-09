4e3c3a1592e4fff15ec989d8af6ae110e82f767a
StevenJiao <syjiao@ualberta.ca>
2019-11-09 16:02:51 -0700

    Added descriptions to getPdfList

--- sylRead/sylRead.py
+++ sylRead/sylRead.py
@@ -3,11 +3,20 @@ from os import listdir, getcwd
 from os.path import isdir, join
 
 def getPdfList():
+	'''
+		getPdfList will search for a folder in the current 
+		directory and go through its PDFs, converting them into
+		a .txt file for parsing.
+	'''
+	# get the current/working directory
 	myPath = getcwd()
+
 	for item in listdir(myPath):
+		# if its a folder, set that to the new path
 		if isdir(join(myPath, item)) and item != "tika-app-python":
 			newPath = join(myPath, item)
 
+	#using this new path, get textfiles of the pdfs
 	for pdf in listdir(newPath):
 		getTextFile(join(newPath, pdf))
 
